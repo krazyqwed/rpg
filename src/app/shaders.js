ba65bdb4c9480df07a057d65abef3d1f1c5fbcb3
Krazyqwed <krazyqwed@gmail.com>
2016-07-05 01:02:32 +0200

    missing shader js

---
+++ src/app/shaders.js
@@ -0,0 +1,22 @@
+export var selectiveColor = `
+  precision mediump float;
+
+  varying vec2 vTextureCoord;
+  varying vec4 vColor;
+
+  uniform sampler2D uSampler;
+  uniform float customUniform;
+
+  void main(void)
+  {
+     vec2 uvs = vTextureCoord.xy;
+
+     vec4 fg = texture2D(uSampler, vTextureCoord);
+
+     //fg.r *= 0.05;
+     //fg.g *= 0.05;
+     //fg.b *= 0.6;
+
+     gl_FragColor = fg;
+  }
+`;
